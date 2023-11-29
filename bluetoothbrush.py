434c728c4b71f40e1730e7d2d9ab688dec75ccf7
Kevin McAleer <kevinmcaleer@gmail.com>
2023-11-29 09:40:14 +0000

    updates

---
+++ bluetoothbrush.py
@@ -0,0 +1,17 @@
+class BluetoothBrush():
+
+    def __init__(self):
+        pass
+        self.display = ssd1306.SSD1306_I2C(128, 64, i2c)
+    # display boot up message
+
+    def motor_on(self):
+        pass
+
+    def motor_off(self):
+        pass
+
+    def motor_speed(self, speed):
+        pass
+
+    
